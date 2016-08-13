93cd95b8eb677ca4e5755721f9486389037b1d3f
Niels de Vos <niels@nixpanic.net>
2016-08-13 22:21:30 +0200

    [WIP] comment out rtn_submit

--- common-scripts/basic-gluster-duffy.py
+++ common-scripts/basic-gluster-duffy.py
@@ -68,8 +68,7 @@ else:
 
 #publish='gerrit review --message "' + os.getenv("BUILD_URL") + 'consoleFull : ' + verdict + '" --project '+ os.getenv("GERRIT_PROJECT") + ' --notify=NONE ' +  os.getenv("GERRIT_PATCHSET_REVISION")
 #result_submit="ssh -l jenkins-glusterorg -i /home/nfs-ganesha/.ssh/gerrithub\@gluster.org -o StrictHostKeyChecking=no -p 29418 review.gerrithub.io '%s' " % (publish)
-
-rtn_submit=subprocess.call(result_submit, shell=True)
+#rtn_submit=subprocess.call(result_submit, shell=True)
 
 # return the system(s) to duffy
 done_nodes_url="%s/Node/done?key=%s&ssid=%s" % (url_base, api, b['ssid'])
